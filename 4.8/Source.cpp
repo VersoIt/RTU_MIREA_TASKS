577e6d25461b55ab5d02b2d47764a616379a8fde
Ruslan <ruslan.itpro@gmail.com>
2022-09-16 15:22:17 +0300

    Updated specific tasks

--- 4.8/Source.cpp
+++ 4.8/Source.cpp
@@ -10,29 +10,13 @@ public:
 	{
 	}
 
-	size_t getRows() const
-	{
-		return m_data.size();
-	}
+	size_t getRowsCount() const;
 
-	size_t getColumns() const
-	{
-		return m_data[0].size();
-	}
+	size_t getColumnsCount() const;
 
-	friend float operator*(Matrix other)
-	{
-		float sum;
-		for (int i = 0; i < other.getRows(); ++i)
-		{
-			for (int j = 0; j < other.getColumns(); ++j)
-			{
-				// TODO: sum += m_data[i][j] * other.getMatrix()[j][i];
-			}
-		}
-	}
+	friend float operator*(const Matrix& current, const Matrix& other);
 
-	std::vector<std::vector<float>>& getMatrix() const
+	const std::vector<std::vector<float>>& getMatrix() const
 	{
 		return m_data;
 	}
@@ -41,13 +25,44 @@ private:
 	std::vector<std::vector<float>>& m_data;
 };
 
+size_t Matrix::getRowsCount() const
+{
+	return m_data.size();
+}
+
+size_t Matrix::getColumnsCount() const
+{
+	return m_data[0].size();
+}
+
+float operator*(const Matrix& current, const Matrix& other)
+{
+	if (current.getColumnsCount() != other.getRowsCount())
+		throw std::exception("Count of columns in first matrix don't equal of count of rows in second matrix.");
+
+	float sum{ 0 };
+
+	for (int k { 0 }; k < other.getColumnsCount(); ++k)
+	{
+		for (int i { 0 }; i < current.getRowsCount(); ++i)
+		{
+			for (int j { 0 }; j < current.getColumnsCount(); ++j)
+			{
+				sum += current.m_data[i][j] * other.m_data[j + k][i];
+			}
+		}
+	}
+
+	return sum;
+}
+
 int main()
 {
 
 	std::vector<std::vector<float>> first_data{ {
-		{5, 2, 0, 10},
-		{3, 5, 2, 5},
-		{20, 0, 0, 0} 
+		{5.f, 2.f, 0.f, 10.f},
+		{3.f, 5.f, 2.f, 5.f},
+		{20.f, 0.f, 0.f, 0.f}
 	} };
 
 	std::vector<std::vector<float>> second_data{ {
@@ -60,12 +75,14 @@ int main()
 	Matrix A(first_data);
 	Matrix B(second_data);
 
-	int a;
-	std::cin >> a;
-	if (std::cin.fail())
-		std::cout << "Îøèáêà" << std::endl;
-	std::cout << a << std::endl;
-
+	try
+	{
+		std::cout << A * B << std::endl;
+	}
+	catch (std::exception e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
 	return EXIT_SUCCESS;
 }
